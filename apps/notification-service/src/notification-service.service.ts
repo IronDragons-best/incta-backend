5b67734d9780342e268e8bc5837dd2eb427693cf
RuslanV93 <ruslanvak93@gmail.com>
2025-06-25 14:15:59 +0300

    fix: tests

--- apps/notification-service/src/notification-service.service.ts
+++ apps/notification-service/src/notification-service.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NotificationServiceService {
   getHello(): string {
-    return 'Hello from notification service!';
+    return 'Hello World!';
   }
 }
