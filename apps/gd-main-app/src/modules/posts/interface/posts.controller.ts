d24bbcc510297b00b1d67fa3703fcd6a7e7df5c8
Andrewdragom <andreasdragomirov@yandex.com>
2025-06-29 15:02:35 +0300

    feature: swagger

--- apps/gd-main-app/src/modules/posts/interface/posts.controller.ts
+++ apps/gd-main-app/src/modules/posts/interface/posts.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { PostsService } from '../application/post.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostsController {
@@ -9,10 +10,13 @@ export class PostsController {
     @Inject(PostsService) protected postsService: PostsService,
   ) {}
   @Get()
+  @ApiResponse({ status: 200, description: 'Success' })
   async getPosts() {
     return this.postsService.findPosts();
   }
   @Get(':id')
+  @ApiResponse({ status: 201, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
   async getPostById(@Param('id') id: string) {
     return await this.postsService.findPostById(id);
   }
--- apps/gd-main-app/src/modules/users/interface/users.controller.ts
+++ apps/gd-main-app/src/modules/users/interface/users.controller.ts
@@ -5,6 +5,7 @@ import { GetUserQuery } from '../application/query-handlers/get.user.query.handl
 import { UserInputDto } from './dto/user.input.dto';
 import { CreateUserCommand } from '../application/use-cases/create.user.use.case';
 import { DeleteUserCommand } from '../application/use-cases/delete.user.use.case';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
@@ -13,19 +14,27 @@ export class UsersController {
     private readonly queryBus: QueryBus,
   ) {}
   @Get()
+  @ApiResponse({ status: 200, description: 'Success' })
   getUsers() {
     return this.queryBus.execute(new GetAllUsersQuery());
   }
   @Get(':id')
+  @ApiResponse({ status: 201, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
   getUserById(@Param('id') id: number) {
     return this.queryBus.execute(new GetUserQuery(id));
   }
 
   @Post()
+  @ApiResponse({ status: 201, description: 'Returns the newly created user' })
+  @ApiResponse({ status: 400, description: 'If the inputModel has incorrect values' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
   createUser(@Body() body: UserInputDto) {
     return this.commandBus.execute(new CreateUserCommand(body));
   }
   @Delete('id')
+  @ApiResponse({ status: 201, description: 'Success' })
+  @ApiResponse({ status: 404, description: 'Not Found' })
   deleteUser(@Param('id') id: number) {
     return this.commandBus.execute(new DeleteUserCommand(id));
   }
