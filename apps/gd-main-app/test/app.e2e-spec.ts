6467cf25f2cdcfc8541ef0a7ed590863c64213a2
RuslanV93 <ruslanvak93@gmail.com>
2025-07-07 20:05:02 +0300

    chore(IN-55): typeorm deps updated

--- apps/gd-main-app/test/app.e2e-spec.ts
+++ apps/gd-main-app/test/app.e2e-spec.ts
@@ -1,20 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { App } from 'supertest/types';
-import { AppModule } from '../src/app.module';
-import request from 'supertest';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  beforeEach(async () => {});
 
   it('/ (GET)', () => {
     expect(1).toBe(1);
