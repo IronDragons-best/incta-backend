85397d5c1166cf5e6b8da9916e00990e36b7ad3e
anovic123 <anovicjosip@gmail.com>
2025-07-14 02:12:37 +0300

    Add JWT auth-based logout functionality and integration tests
    - Implement `JwtAuthGuard` and `JwtStrategy` for validating JWTs.
    - Integrate logout endpoint in `AuthController` with cookie clearance using `COOKIE_OPTIONS`.
    - Add Swagger decorators for logout API documentation.
    - Write integration tests for logout functionality in `AuthController`.
    - Update dependencies to include `@types/passport-jwt`.

---
+++ apps/gd-main-app/core/decorators/swagger-settings/logout.swagger.decorator.ts
@@ -0,0 +1,18 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ErrorResponseDto } from '@common';
+
+export function LogoutSwagger() {
+  return applyDecorators(
+    ApiOperation({ summary: 'User Logout.' }),
+    ApiResponse({
+      status: HttpStatus.NO_CONTENT,
+      description: 'Logout successful, no content returned',
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      type: ErrorResponseDto,
+      description: 'Logout failed. User is not authenticated.',
+    })
+  );
+}
