f3fb00c8fc709108a8e7c765f948473af70460fd
anovic123 <anovicjosip@gmail.com>
2025-07-15 13:00:32 +0300

    Add imports in AuthController
    - Imported additional dependencies, including `AuthService`, guards, DTOs, and Swagger decorators, to extend functionality for authentication-related endpoints.

--- apps/gd-main-app/src/modules/auth/interface/auth.controller.ts
+++ apps/gd-main-app/src/modules/auth/interface/auth.controller.ts
@@ -23,6 +23,14 @@ import { LocalAuthGuard } from '../../../../core/guards/local/local.auth.guard';
 import { CookieInterceptor } from '../../../../core/interceptors/refresh-cookie.interceptor';
 import { TokenResponseDto } from '../../../../core/types/token.types';
 import { LoginSwagger } from '../../../../core/decorators/swagger-settings/login.swagger.decorator';
+import { AuthService } from '../application/auth.service';
+import { MeSwagger } from '../../../../core/decorators/swagger-settings/me.swagger.decorator';
+import { JwtAuthGuard } from '../../../../core/guards/local/jwt-auth-guard';
+import { AuthMeViewDto } from './dto/output/me.view.dto';
+import { COOKIE_OPTIONS } from '../constants/cookie-options.constants';
+import { LogoutSwagger } from '../../../../core/decorators/swagger-settings/logout.swagger.decorator';
+import { EmailResendCommand } from '../application/use-cases/email.resend.use-case';
+import { EmailResendInputDto } from './dto/email.resend.input.dto';
 
 @Controller('auth')
 export class AuthController {
