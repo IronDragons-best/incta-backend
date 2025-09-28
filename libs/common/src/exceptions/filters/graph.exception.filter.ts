5a8bb91f4afaba5ac9e4c2b999fadea2ae3bd7ac
RuslanV93 <ruslanvak93@gmail.com>
2025-09-28 13:29:22 +0300

    graphql fix

--- libs/common/src/exceptions/filters/graph.exception.filter.ts
+++ libs/common/src/exceptions/filters/graph.exception.filter.ts
@@ -2,27 +2,15 @@ import { ArgumentsHost, Catch, Logger } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql/error';
 
-@Catch()
+@Catch(GraphQLError)
 export class GraphQLExceptionFilter implements GqlExceptionFilter {
   private readonly logger = new Logger(GraphQLExceptionFilter.name);
 
-  catch(exception: any, host: ArgumentsHost): GraphQLError {
-    // Логируем ошибку для отладки
+  catch(exception: GraphQLError, host: ArgumentsHost): GraphQLError {
+    // Логируем только графовые ошибки
     this.logger.error('GraphQL Exception:', exception);
 
-    // Если это уже GraphQLError, просто возвращаем
-    if (exception instanceof GraphQLError) {
-      return exception;
-    }
-
-    // Для всех остальных ошибок создаем GraphQLError
-    const message = exception?.message || 'Internal server error';
-
-    return new GraphQLError(message, {
-      extensions: {
-        code: 'INTERNAL_SERVER_ERROR',
-        originalError: exception?.name || 'UnknownError',
-      },
-    });
+    // Просто возвращаем как есть
+    return exception;
   }
 }
